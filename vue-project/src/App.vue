683e8d442308ca4ea6748315aede2fd7eb4df4d1
laurahohlsiepe <l.hohlsiepe@gmail.com>
2023-08-07 12:37:30 +0200

    git init

---
+++ vue-project/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<body class="bg-pink-300 font-mono h-screen">
+  <header>
+    <h1 class="text-2xl text-center pt-12">Sun Sign App</h1>
+    <p class="text-md text-center pt-6">Enter your date of birth and find out your zodiac sign and info about it 💓 </p>
+  </header>
+  <main>
+    <section class="pt-12">
+      <p class="text-center py-2">Enter your birthdate</p>
+      <div class="flex justify-center py-2">
+        <input type="date" class="border border-gray-300 text-pink-600 text-lg rounded-lg">
+      </div>
+    </section>
+  </main>
+</body>
+</template>
+
+<script>
+
+</script>
+
